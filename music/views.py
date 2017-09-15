4db9aa54896c95a456db3d3cafef3cfe7c90adf8
takanarisasaki <takanari.s@hotmail.com>
2017-09-14 22:26:12 -0400

    Creating and separating templates from views

--- music/views.py
+++ music/views.py
@@ -1,27 +1,45 @@
 # Views are simply python functions that take user request and give a response in some kind of way.
 # Most of the time, the user is just going to request a webpage and just give a webpage.
 # Each URL in urls.py is connected to a view, i.e. each URL is connected to an HTML response.
+# Whenever we have a view, we need to return something.
 
 from django.http import HttpResponse
 # Since we are going to work with Album information, we need to import it.
 from .models import Album
+from django.template import loader
 
-# request is an HTML request
+# # request is an HTML request
+# def index(request):
+#     # First, we need to connect to the database and get all of the Albums.
+#     # all_albums will store the results of the database call.
+#     # Album.objects.all() will connect to the database, look inside Album table, and store it inside all_albums
+#     all_albums = Album.objects.all()
+#     html = ''
+#
+#     # Now that we have a list of albums, we need to loop through them so that we can access them one by one.
+#     for album in all_albums:
+#         # For each album, we want to make a link.
+#         # Cannot concatenate integer with string, so need to convert to string first.
+#         url = '/music/' + str(album.id) + '/'
+#         html += '<a href=" ' + url + ' ">' + album.album_title + '</a> <br>'
+#
+#     return HttpResponse(html)
+
+
+# Using template to separate our front-end code from back-end code
+# First, make a template file in /music/templates/music/index.html
 def index(request):
-    # First, we need to connect to the database and get all of the Albums.
-    # all_albums will store the results of the database call.
-    # Album.objects.all() will connect to the database, look inside Album table, and store it inside all_albums
     all_albums = Album.objects.all()
-    html = ''
+    template = loader.get_template('music/index.html')
 
-    # Now that we have a list of albums, we need to loop through them so that we can access them one by one.
-    for album in all_albums:
-        # For each album, we want to make a link.
-        # Cannot concatenate integer with string, so need to convert to string first.
-        url = '/music/' + str(album.id) + '/'
-        html += '<a href=" ' + url + ' ">' + album.album_title + '</a> <br>'
+    # Whenever we pass all_albums information to our template (music/index.html),
+    # we pass it through a dictionary. We often name that dictionary "context".
+    context = {
+        'all_albums': all_albums,
+    }
 
-    return HttpResponse(html)
+    return HttpResponse(template.render(context, request))
+
 
 # album_id came from music/urls.py
 # So whenever this function is called, it returns this HTTP response.
